c3ed9bcbcdfa16fc057884d59ee1d2b76ef527dc
guwoh <hoanghiep2k5@gmail.com>
2024-07-11 20:29:31 +0700

    update

--- CameraFaceRecognitionDoorLock/CameraFaceRecognitionDoorLock.ino
+++ CameraFaceRecognitionDoorLock/CameraFaceRecognitionDoorLock.ino
@@ -1,18 +1,7 @@
-//Viral Science www.youtube.com/c/viralscience  www.viralsciencecreativity.com
-//ESP Camera Artificial Intelligence Face Detection Automatic Door Lock
 #include "esp_camera.h"
 #include <WiFi.h>
 
-//
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-//
 
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
-//#define CAMERA_MODEL_ESP_EYE
-//#define CAMERA_MODEL_M5STACK_PSRAM 
-//#define CAMERA_MODEL_M5STACK_WIDE
 #define CAMERA_MODEL_AI_THINKER
 #define Relay 2
 
